8358d52a692d407cea217218e8067b557e9d85d7
Hải <85049064+mountto@users.noreply.github.com>
2026-02-26 23:11:50 +0700

    Update InstanceId.cmd

--- InstanceId.cmd
+++ InstanceId.cmd
@@ -8,6 +8,6 @@ rem */
 @echo off
 rem "lists all SAMSUNG Mobile USB Composite Device Android devices connected to a computer via USB or Wi-Fi and redirects its output to a new file named InstanceId.txt"
 
-powershell "Get-PnpDevice -PresentOnly -Class USB | Where-Object { $_.InstanceId -match '^USB\\VID_04E8&PID_6860' } | Select-Object @{n='InstanceID';e={$_.InstanceId -replace '^PCI\\(.*?)&REV.*$','$1'}}" > InstanceId.txt
+powershell "Get-PnpDevice -PresentOnly -Class USB | Where-Object { $_.InstanceId -match '^USB\\VID_04E8&PID_6860' } | Select-Object @{n='InstanceID';e={$_.InstanceId -replace '^PCI\\(.*?)&REV.*$','$1'}}" > \\your path\InstanceId.txt
 echo "FIN"
 pause
